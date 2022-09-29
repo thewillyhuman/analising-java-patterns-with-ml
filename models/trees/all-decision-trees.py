9a5a743e72a1c429a89d6264b78b78bb4202fbc8
thewillyhuman <colunga91@gmail.com>
2022-09-29 12:53:22 +0200

    revision

--- models/trees/all-decision-trees.py
+++ models/trees/all-decision-trees.py
@@ -1,26 +1,27 @@
 # -*-encoding:utf-8-*-
 
 
-from sklearn import tree
-import matplotlib.pyplot as plt
-import graphviz
-from dtreeviz.trees import dtreeviz
 import os
+from typing import List
+
+import graphviz
+import matplotlib.pyplot as plt
 import pandas as pd
-from typing import List, Set, Any, Union
+from dtreeviz.trees import dtreeviz
+from sklearn import tree
 
-from sklearn.metrics import accuracy_score
-
-from configuration import get_all_the_feature_names
-from util import load_file, convert_to_one_hot, convert_boolean_features, show_accuracies, cross_validation
-
+from util import load_file, show_accuracies, \
+    cross_validation
 
 TREE_FIGURES_DIR = "tree/"
-CSVS_DIR = "csvs/"
-CSV_FILE_NAMES = ["expressions_unique.csv", "field_defs_unique.csv", "method_defs_unique.csv", "programs_unique.csv",
-                  "statements_unique.csv", "type_defs_unique.csv", "types_unique.csv",
-                  "het1_unique.csv", "het2_unique.csv", "het3_unique.csv", "het4_unique.csv", "het5_unique.csv"]
-
+CSVS_DIR = "../../data/unique/"
+CSV_FILE_NAMES = ["programs/dataset_and_target.csv", "type_defs/dataset_and_target.csv",
+                  "field_defs/dataset_and_target.csv",
+                  "method_defs/dataset_and_target.csv", "expressions/dataset_and_target.csv",
+                  "statements/dataset_and_target.csv", "types/dataset_and_target.csv",
+                  "het1/dataset_and_target.csv", "het2/dataset_and_target.csv",
+                  "het3/dataset_and_target.csv", "het4/dataset_and_target.csv",
+                  "het5/dataset_and_target.csv", ]
 
 MIN_TREE_DEPTH = 1
 MAX_TREE_DEPTH = 3
@@ -54,7 +55,8 @@ def render_tree_graphviz(clf, feature_manes: List[str], class_names: List[str], 
     graph.render(TREE_FIGURES_DIR + file_name)
 
 
-def render_tree_dtreeviz(clf, feature_manes: List[str], class_names: List[str], target_name: str, file_name: str):
+def render_tree_dtreeviz(clf, feature_manes: List[str], class_names: List[str], target_name: str,
+                         file_name: str):
     viz = dtreeviz(clf, X.values, y.values.reshape(-1),
                    target_name=target_name,
                    class_names=class_names,
@@ -68,22 +70,30 @@ def show_rules(clf, feature_names: List[str]):
         print(text_representation)
 
 
-def show_trees(clf, feature_names: List[str], class_names: List[str], target_feature: str, file_name_prefix: str):
+def show_trees(clf, feature_names: List[str], class_names: List[str], target_feature: str,
+               file_name_prefix: str):
     if not os.path.exists(TREE_FIGURES_DIR):
         os.makedirs(TREE_FIGURES_DIR)
-    render_tree_plt(clf, list(feature_names), class_names, f"{file_name_prefix}-plt.png")
-    render_tree_graphviz(clf, list(feature_names), class_names, f"{file_name_prefix}-graphviz.png")
-    render_tree_dtreeviz(clf, list(feature_names), class_names, target_feature, f"{file_name_prefix}-dtreeviz.svg")
+    # render_tree_plt(clf, list(feature_names), class_names, f"{file_name_prefix}-plt.png")
+    # render_tree_graphviz(clf, list(feature_names), class_names, f"{file_name_prefix}-graphviz.png")
+    # render_tree_dtreeviz(clf, list(feature_names), class_names, target_feature,
+    # f"{file_name_prefix}-dtreeviz.svg")
 
 
 if __name__ == "__main__":
 
     for csv_file_name in CSV_FILE_NAMES:
         file_name = os.path.splitext(csv_file_name)[0]  # file name without the extension
-        print("\n", "-"*10, f"Processing '{file_name}' file", "-"*10, "\n")
+        print("\n", "-" * 10, f"Processing '{file_name}' file", "-" * 10, "\n")
         # load file
         print("loading data...")
         df, X, y = load_file(CSVS_DIR + csv_file_name, None, TARGET_FEATURE, SHOW_TABLES)
+        X = pd.get_dummies(X)
+        X = X.astype('float32')
+        X = X.fillna(0.0)
+        y = y.squeeze()
+        y = y.apply(lambda value: 0 if value == "low" else 1)  # high will be 1 and low will be 0.
+        y = y.astype('float32')
         feature_names = list(X.columns)
 
         for max_depth in range(MIN_TREE_DEPTH, MAX_TREE_DEPTH + 1):
@@ -106,4 +116,4 @@ if __name__ == "__main__":
             show_accuracies(clf, df, X, y, TARGET_FEATURE, SHOW_ACCURACIES)
             cross_validation(clf, X, y, SHOW_ACCURACIES, 10, SHOW_TABLES)
             show_rules(clf, feature_names)
-            show_trees(feature_names, CLASS_NAMES, TARGET_FEATURE, f"tree-{file_name}-no-limit")
+            # show_trees(feature_names, CLASS_NAMES, TARGET_FEATURE, f"tree-{file_name}-no-limit")
